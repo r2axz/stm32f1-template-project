8010743ef54f264c21af5e1d6c428b1a7e73d7ed
Kirill Kotyagin <kirill@kwire.net>
2019-09-24 20:07:30 +0300

    ADD: blink led at PC13

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+#include <stm32f1xx.h>
 #include "sys_init.h"
+
+void dummy_delay(unsigned int count) {
+    volatile unsigned int _count = count;
+    while(_count--);
+}
 
 int main() {
     sys_configure_clock();
+    // Enable GPIOC clock
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    GPIOC->CRH &= ~GPIO_CRH_CNF13;
+    GPIOC->CRH |= GPIO_CRH_MODE13_1;
+    while (1) {
+        GPIOC->BSRR = GPIO_BSRR_BR13;
+        dummy_delay(9000000);
+        GPIOC->BSRR = GPIO_BSRR_BS13;
+        dummy_delay(9000000);
+    }
     return 0;
 }
